7d84f282e2ee249ce4f1ff460958ab35650b0bf6
陈以泉 <691980967@qq.com>
2019-08-07 15:45:06 +0800

    nginx反向代理

--- src/router/blog.js
+++ src/router/blog.js
@@ -1,11 +1,29 @@
 const { getList, getDetail, newBlog, updateBlog, deleteBlog } = require('../controller/blog')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 
+/**
+ * 统一的登录验证函数
+ * @param req
+ * @returns Promise
+ */
+const loginCheck = req => {
+  if (!req.session.username) {
+    return Promise.resolve(new ErrorModel('尚未登录'))
+  }
+}
 
+/**
+ * 路由
+ * @param req
+ * @param res
+ * @returns PromiseLike
+ */
 const handleBlogRouter= (req, res) => {
-  const { method, url } = req
+  const { method } = req
   const path = req.path
   const { author, keyword, id } = req.query
+  const loginCheckResult = loginCheck(req)
+
 
   // 获取博客列表
   if (method === 'GET') {
@@ -25,6 +43,10 @@ const handleBlogRouter= (req, res) => {
   } else {
     switch (path) {
       case '/api/blog/new':
+        if (loginCheckResult) {
+          // 未登录
+          return loginCheck
+        }
         // mock登录人
         req.body.author = '梨'
         const newRs = newBlog(req.body)
@@ -32,11 +54,21 @@ const handleBlogRouter= (req, res) => {
           return new SuccessModel(data)
         })
       case '/api/blog/update':
+        if (loginCheckResult) {
+          // 未登录
+          return loginCheck
+        }
+
         const updateRs = updateBlog(id, req.body)
         return updateRs.then(data => {
           return data ? new SuccessModel(data) : new ErrorModel('更新博客失败')
         })
       case '/api/blog/delete':
+        if (loginCheckResult) {
+          // 未登录
+          return loginCheck
+        }
+
         const author = '梨'
         const delRs = deleteBlog(id, author)
         return delRs.then(data => {
--- src/router/user.js
+++ src/router/user.js
@@ -1,16 +1,27 @@
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 const { loginCheck } = require('../controller/user')
+const { set } = require('../db/redis')
 
 const handleUserRouter = (req, res) => {
   const { method } = req
+
+  // 登录
   if (method === 'POST' && req.path === '/api/user/login') {
     const { username, pwd } = req.body
     const rsData = loginCheck(username, pwd)
 
     return rsData.then(result => {
-      if (result) {
-        return new SuccessModel(result)
+      if (result.username) {
+        // 设置session
+        req.session.username = result.username
+        req.session.realname = result.realname
+
+        // 同步到redis
+        set(req.sessionId, req.session)
+
+        return new SuccessModel()
       }
+
       return new ErrorModel('登录失败')
     })
 
